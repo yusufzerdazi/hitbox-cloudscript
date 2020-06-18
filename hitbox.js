4c493a705e079bed3e0008c2206d3db1362671cc
Yusuf Zerdazi <yusuf@zerdazi.com>
2020-06-18 21:17:57 +0100

    Update hitbox.js

--- hitbox.js
+++ hitbox.js
@@ -1,1 +1,18 @@
-
+handlers.playerWins = function (args) {
+    var updateStatistics = server.UpdatePlayerStatistics({
+    PlayFabId: currentPlayerId,
+    Statistics: [{
+      "StatisticName": "wins",
+      "Value": 1
+    }]
+  });
+};
+handlers.playerLoses = function (args) {
+    var updateStatistics = server.UpdatePlayerStatistics({
+    PlayFabId: currentPlayerId,
+    Statistics: [{
+      "StatisticName": "losses",
+      "Value": 1
+    }]
+  });
+};
