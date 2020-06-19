59fe80af40525c0633cc65dfd897d6cf17e27386
Yusuf Zerdazi <yusuf@zerdazi.com>
2020-06-19 02:19:26 +0100

    Update hitbox.js

--- hitbox.js
+++ hitbox.js
@@ -1,18 +1,48 @@
 handlers.playerWins = function (args) {
     var updateStatistics = server.UpdatePlayerStatistics({
-    PlayFabId: currentPlayerId,
-    Statistics: [{
-      "StatisticName": "wins",
-      "Value": 1
-    }]
-  });
+        PlayFabId: currentPlayerId,
+        Statistics: [{
+          "StatisticName": "wins",
+          "Value": 1
+        }]
+    });
+    var currentStatistics = server.GetPlayerStatistics({
+        PlayFabId: currentPlayerId,
+        StatisticNames: ["wins", "losses"]
+    });
+    var wins = currentStatistics.filter(s => s.StatisticName == "wins");
+    var losses = currentStatistics.filter(s => s.StatisticName == "losses");
+    if(wins && losses){
+        var updateStatistics = server.UpdatePlayerStatistics({
+            PlayFabId: currentPlayerId,
+            Statistics: [{
+              "StatisticName": "winloss",
+              "Value": wins / losses
+            }]
+        });
+    }
 };
 handlers.playerLoses = function (args) {
     var updateStatistics = server.UpdatePlayerStatistics({
-    PlayFabId: currentPlayerId,
-    Statistics: [{
-      "StatisticName": "losses",
-      "Value": 1
-    }]
-  });
+        PlayFabId: currentPlayerId,
+        Statistics: [{
+          "StatisticName": "losses",
+          "Value": 1
+        }]
+    });
+    var currentStatistics = server.GetPlayerStatistics({
+        PlayFabId: currentPlayerId,
+        StatisticNames: ["wins", "losses"]
+    });
+    var wins = currentStatistics.filter(s => s.StatisticName == "wins");
+    var losses = currentStatistics.filter(s => s.StatisticName == "losses");
+    if(wins && losses){
+        var updateStatistics = server.UpdatePlayerStatistics({
+            PlayFabId: currentPlayerId,
+            Statistics: [{
+              "StatisticName": "winloss",
+              "Value": wins / losses
+            }]
+        });
+    }
 };
